51a6587eed3eb3973779f316482390839f6257e5
exxon suarez <suarez.exxon@gmail.com>
2023-10-09 16:36:53 -0700

    finished click logic for toggling on red fill and table with associated zone data

--- test_map/src/Layout.jsx
+++ test_map/src/Layout.jsx
@@ -3,7 +3,7 @@ import  "./sass/style.scss";
 
 const Layout = ({children}) => {
   return (
-    <div className="layout container-fluid vh-100">
+    <div className="layout container-fluid vh-100 g-0">
       {children}
     </div>
   )
--- test_map/src/components/Header.jsx
+++ test_map/src/components/Header.jsx
@@ -4,26 +4,9 @@ import  "../sass/style.scss";
 
 export default function Header() {
     return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-        <a class="navbar-brand" href="#">Map Test</a>
-
-        <div class="" id="navbarNav">
-            <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-            </ul>
-        </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+        <a className="navbar-brand" href="#">Map Test</a>
         </div>
     </nav>
     )
