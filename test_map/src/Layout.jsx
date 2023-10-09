140ee2e0971783a8c460d023676abfbd04b3ce36
exxon suarez <suarez.exxon@gmail.com>
2023-10-09 03:42:38 -0700

    created map component and added function to generate random color hex codes for svg paths

--- test_map/src/Layout.jsx
+++ test_map/src/Layout.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import  "./sass/style.scss";
 
 const Layout = ({children}) => {
   return (
-    <div className="layout">
+    <div className="layout container-fluid vh-100">
       {children}
     </div>
   )
