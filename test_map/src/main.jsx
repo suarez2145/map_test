89cf27dcf913605e6a52459cd2b6bde7d34217e1
exxon suarez <suarez.exxon@gmail.com>
2023-10-09 01:43:05 -0700

    added react and imported svg map

--- test_map/src/main.jsx
+++ test_map/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Header from "./components/Header";
+import Layout from './Layout.jsx'
+import Map from "./components/Map";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <Layout>
+    <Header/>
+    <Map/>
+  </Layout>
+);
